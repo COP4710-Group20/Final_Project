6721be91449e4ca8e16b5f1fe14ae01380ed080e
goodguytim1 <timobrien56@knights.ucf.edu>
2020-12-04 12:29:34 -0500

    Add inputs for registration/login

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,10 +1,21 @@
 import './App.css';
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Login() {
   return (
     <div>
      <h1> Login </h1>
+     <form className="loginForm">
+         <label> Username </label>
+         <input type="text" placeholder="Enter Username..."/>
+         <label>Password</label>
+         <input type="password" placeholder="Enter Password..."/>
+         <input type="submit" className="login-submit" Value="login"/>
+         <Link to="/register">
+         <p>Don't have an account?</p>
+         </Link>
+     </form>    
     </div>
   );
 }

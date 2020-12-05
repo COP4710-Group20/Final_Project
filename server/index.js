9281dbbc44b43df4ba98895e33115c5b931d1434
goodguytim1 <timobrien56@knights.ucf.edu>
2020-12-04 21:40:32 -0500

    Inserted into DB

--- server/index.js
+++ server/index.js
@@ -2,19 +2,25 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'CrudDB'
+    database: 'myschema'
 });
 
+
+
 app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Inception', 'Good Movie');";
-    db.query(sqlInsert, (err, result) => {
-        res.send("Hello World");
-    })
-    
+    db.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Inception', 'Good Movie')";
+        db.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+        });
+      });
 });
 
 app.listen(3001, () => {

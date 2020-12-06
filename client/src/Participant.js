fef65062d165cc2ea64a522f214ff5014fece729
Ipan98 <70811372+Ipan98@users.noreply.github.com>
2020-12-06 14:04:36 -0500

    Update Participant.js

--- client/src/Participant.js
+++ client/src/Participant.js
@@ -1,17 +1,32 @@
 import React from "react";
+import  {useState, useEffect} from 'react';
 import {useHistory} from "react-router-dom";
 import "./Participant.css";
+import Axios from 'axios';
 
-function Participant() { 
-    const history=useHistory(); 
+function Participant() {
+    const [name, setUser]= useState(false);
+    useEffect(() => {
+    getName();
+  }, []);
+    const history=useHistory();
     const routeParticipant= () =>{
-        // let path ='participant'; 
-        history.push('/participant'); 
+        // let path ='participant';
+        history.push('/participant');
     }
     const routeAdmin= () =>{
-        // let path ='participant'; 
-        history.push('/adminview'); 
+        // let path ='participant';
+        history.push('/adminview');
     }
+    function getName() {
+    fetch('http://localhost:3001/participant')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        setName(data);
+      });
+  }
     return (
         <div className="containerB">
             <div className="Top_column">
@@ -21,24 +36,18 @@ function Participant() {
                     <button onClick={routeParticipant}>View Participants</button>
                     <button onClick={routeAdmin}>View Admin</button>
                 </div>
-                
-            </div>        
-            <div className="column"> 
-                <h2>Participant Name</h2>
-                    Emmanuel
-                    
-               
+
             </div>
-            <div className="column"> 
-                <h2>Email</h2>
-                    Emmanuel@example.com
+            <div className="column">
+                <h2>Participant Name</h2>
+                    {name ? name : 'Empty'}
             </div>
 
-            <div className="column"> 
+            <div className="column">
                 <h2>Events Attended</h2>
                     1
             </div>
-            
+
         </div>
     )
 }

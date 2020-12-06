a0ed4c5d8aee52ff2664394ccda19c75152c3a3f
Ipan98 <70811372+Ipan98@users.noreply.github.com>
2020-12-06 14:21:17 -0500

    Update AdminView.js

--- client/src/AdminView.js
+++ client/src/AdminView.js
@@ -1,15 +1,29 @@
 import React from "react";
+import  {useState, useEffect} from 'react';
 import {useHistory} from "react-router-dom";
 import "./AdminView.css";
 
 function AdminView() {
-    const history=useHistory(); 
+    const [name, setName]= useState(false);
+    useEffect(() => {
+    getName();
+  }, []);
+    const history=useHistory();
     const routeParticipant= () =>{
-        history.push('/participant'); 
+        history.push('/participant');
     }
     const routeAdmin= () =>{
-        history.push('/adminview'); 
+        history.push('/adminview');
     }
+    function getName() {
+    fetch('http://localhost:3001/adminview')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        setName(data);
+      });
+  }
     return (
         <div className="AcontainerB">
             <div className="ATop_column">
@@ -19,24 +33,24 @@ function AdminView() {
                     <button onClick={routeParticipant}>View Participants</button>
                     <button onClick={routeAdmin}>View Admin</button>
                 </div>
-                
-            </div>        
-            <div className="Acolumn"> 
-                <h2>Admin Name</h2>
-                    Emmanuel
+
             </div>
-            <div className="Acolumn"> 
+            <div className="Acolumn">
+                <h2>Admin Name</h2>
+                    {name ? name : 'Empty'}
+            </div>
+            <div className="Acolumn">
                 <h2>Email</h2>
                 <div className="Arowb">
                     emmanuel@example.com
                 </div>
             </div>
 
-            <div className="Acolumn"> 
+            <div className="Acolumn">
                 <h2>Events hosted</h2>
                     10
             </div>
-            
+
         </div>
     )
 }

05cdf553fbe42689d2ebbf3bbc82871abfd0f2d1
truonguyen <truongvnguyen123@gmail.com>
2020-12-09 15:17:45 -0500

    update welcome

--- client/src/User.js
+++ client/src/User.js
@@ -1,10 +1,16 @@
-import './App.css';
-import React, { useEffect } from 'react';
-import {Link} from 'react-router-dom'
-import PartButtons from './PartButtons'
+import React, {useContext} from "react";
+import  {useState, useEffect} from 'react';
+import {Link, useHistory} from "react-router-dom";
 import Axios from 'axios';
+import {HomeContext} from './provider/provider';
+import PartButtons from "./PartButtons";
+
 
 function User() {
+  const global = useContext(HomeContext)
+  const state = global.state
+  const dispatch = global.dispatch
+
   useEffect(() => {
     Axios.get("http://localhost:3001/login").then((response) => {
       console.log(response);
@@ -13,7 +19,7 @@ function User() {
   return (
     <div className="userDashboard">
       <PartButtons></PartButtons>
-     <h1> Welcome User </h1>
+     <h1> Welcome User {state.user.user.display_name} </h1>
     </div>
   );
 }
